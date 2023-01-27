7895bdd9e738134199c743653f0e16b694e4db0f
Mahidhar Reddy Narala <89090086+immahidhar@users.noreply.github.com>
2023-01-27 11:23:21 -0500

    cleanup

--- Project 1/mytime.c
+++ Project 1/mytime.c
@@ -11,31 +11,30 @@
 */
 int executeCommand(char *argv[]) {
     int pid = 0;
-    if ((pid = fork()) == -1) {
-        printf("Error while forking!");
-        return -1;
-    }
     struct rusage childUsage;
     struct timeval childStime, childUtime, childBegin, childEnd;
     gettimeofday(&childBegin, 0);
+    if ((pid = fork()) == -1) {
+        fprintf(stderr, "Error while forking!");
+        return -1;
+    }
     if (pid == 0) {
         // Child process
         // execute
         execvp(argv[1], &argv[1]);
-        printf("Unknown command - %s\n", argv[1]);
+        fprintf(stderr, "Command not found - %s\n", argv[1]);
         exit(-1);
     } else {
         // parent process
         waitpid(pid, NULL, 0);
+        gettimeofday(&childEnd, 0);
         getrusage(RUSAGE_CHILDREN, &childUsage);
         childUtime = childUsage.ru_utime;
         childStime = childUsage.ru_stime;
-        gettimeofday(&childEnd, 0);
-        long seconds = childEnd.tv_sec - childBegin.tv_sec;
-        long microseconds = childEnd.tv_usec - childBegin.tv_usec;
-        double elapsed = seconds + microseconds*1e-6;
-        printf("%s  %ld.%.6ds user  %ld.%.6ds system  %.6fs elapsed\n", argv[1], childUtime.tv_sec, 
-        (childUtime.tv_usec), childStime.tv_sec, (childStime.tv_usec), elapsed);
+        long elapsedSeconds = childEnd.tv_sec - childBegin.tv_sec;
+        long elapsedMicroseconds = childEnd.tv_usec - childBegin.tv_usec;
+        printf("%s  %ld.%.6ds user  %ld.%.6ds system  %ld.%.6lds elapsed\n", argv[1], childUtime.tv_sec, 
+        childUtime.tv_usec, childStime.tv_sec, childStime.tv_usec, elapsedSeconds, elapsedMicroseconds);
     }
     return 0;
 }
@@ -46,7 +45,7 @@ int executeCommand(char *argv[]) {
  */
 int main(int argc, char *argv[]) {
     if(argc == 1) {
-        printf("No command provided to time");
+        fprintf(stderr, "No command provided to time");
         return -1;
     } else {
         return executeCommand(argv);
